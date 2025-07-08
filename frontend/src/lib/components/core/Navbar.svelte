5262e854a5316bd57f44dc01a3872b6f7f50cd51
idke64 <deliciousbrain42@gmail.com>
2025-07-08 12:45:49 -0400

    frontend upadtes

---
+++ frontend/src/lib/components/core/Navbar.svelte
@@ -0,0 +1,12 @@
+<nav class="w-full h-[56px] border-b border-gray-200">
+    <div class="w-full flex justify-between items-center px-8 h-full">
+        <a href="/">
+            <h3>FindWorks</h3>
+        </a>
+        <div class="flex gap-x-6">
+            <a class="nav-link" href="/swipe">
+                Swipe
+            </a>
+        </div>
+    </div>
+</nav>
--- frontend/src/lib/components/ui/PeopleCard.svelte
+++ frontend/src/lib/components/ui/PeopleCard.svelte
@@ -1,4 +1,4 @@
-<div class="w-full flex flex-col rounded-lg border-2 border-gray-200 p-4">
+<div class="w-full flex flex-col rounded-lg border-2 border-gray-200 p-4 self-start">
     <div class="flex flex-col items-center gap-y-4">
         <div class="rounded-lg bg-gray-400 aspect-square w-full">
 
