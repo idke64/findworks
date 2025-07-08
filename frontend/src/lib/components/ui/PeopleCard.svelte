05a30bd0c9b6474c0fdb156c19ce5fdd92aebe1a
idke64 <deliciousbrain42@gmail.com>
2025-07-08 11:06:18 -0400

    frontend

---
+++ frontend/src/lib/components/ui/PeopleCard.svelte
@@ -0,0 +1,9 @@
+<div class="w-full flex flex-col rounded-lg border-2 border-gray-200 p-4">
+    <div class="flex flex-col items-center gap-y-4">
+        <div class="rounded-lg bg-gray-400 aspect-square w-full">
+
+        </div>
+        <h3>Joe</h3>
+        <p>Friend</p>
+    </div>
+</div>
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import PeopleCard from "$lib/components/ui/PeopleCard.svelte";
+
+</script>
+<section class="w-full h-[100vh] flex flex-col items-center justify-center px-64 py-16 gap-8">
+    <div class="flex gap-y-4 flex-col justify-center w-full">
+        <h1 class="text-center flex justify-center">FindWorks</h1>
+        <input placeholder="Interests, Tools, Languages, Teams, Projects" class="w-full border-2 rounded-sm focus:ring-4 focus:ring-blue-400 border-gray-200 px-2 py-1 duration-200"/>
+    </div>
+    <div class="w-full border-2 border-gray-200 h-full rounded-sm grid-cols-3 grid p-6">
+        <PeopleCard/>
+        <PeopleCard/>
+        <PeopleCard/>
+    </div>
+</section>
